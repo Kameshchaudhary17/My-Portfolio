d5d39732ba3f40eefbc8a3ec51c7f39adbc62c78
Kamesh Chaudhary <kameshchaudhary17@gmail.com>
2026-04-17 09:22:04 +0545

    deployment fix

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { DATA } from '../data';
-import { ExternalLink, Github } from 'lucide-react';
 
 export default function Projects() {
   const container = {
